1bd1edfe181d7b51066e989c4fe36e484f2004ca
Jesus <perez.jesus1039@gmail.com>
2023-04-18 16:20:53 -0700

    created the method that accepts content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => console.error('putDb not implemented');
+  const db = await initdb();
+  const tx =db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content});
+  const result = await request;
+  console.log('Data has been saved to the database');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');

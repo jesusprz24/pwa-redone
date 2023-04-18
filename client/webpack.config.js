3c07d7d8d788613add8ccc93d9c0facc7ef51ffc
Jesus <perez.jesus1039@gmail.com>
2023-04-18 16:31:54 -0700

    adding the correct path since i deleted the develop folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = () => {
         publiu_path: '/',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },

f54b2e1e2fe1d6d378983313fb562f19e4c327e0
Jesus <perez.jesus1039@gmail.com>
2023-04-18 15:53:59 -0700

    i thing this is the proper way to implement the asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,5 +28,25 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute();
+const assetCache = new CacheFirst({
+  cacheName: 'asset-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
 
-  
+registerRoute(
+  ({ request }) => request.destination === 'script' || request.destination === 'style',
+  assetCache
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  assetCache
+);
+
